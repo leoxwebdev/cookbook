ffd65ae7257175edb269471f9b457768f5483b2d
Nybble <ne4to777@gmail.com>
2014-10-29 20:14:30 +0300

    server side commit

--- app.js
+++ app.js
@@ -1,36 +1,44 @@
+// requires
 var express = require('express');
-var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
+var router = require('./routes')
+	.router;
+// variables
 var app = express();
 
+// server starts
+app.disable('x-powered-by')
+	.disable('etag')
+	.listen(3000);
+app.use(express.static(__dirname + '/public'));
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
+
+// middleware
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(methodOverride('_method'));
 
-app.use('/', routes);
-app.use('/users', users);
+// router
+app.use(router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handlers
@@ -38,24 +46,21 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
 });
-
-
-module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,211 @@
 var express = require('express');
+var Recipe = require('../db')
+	.Recipe;
+var Category = require('../db')
+	.Category;
+var async = require('async');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+	async.parallel([
+
+			getCategories,
+			function(callback) {
+				getRecipesByCategory(null, function(err, recipes) {
+					if (err) return err;
+					callback(null, recipes);
+				});
+			}
+		],
+		function(err, results) {
+			if (err) return err;
+			res.render('index', {
+				title: "Cookbook",
+				categories: results[0],
+				recipes: results[1]
+			})
+		})
+});
+router.route('/new')
+	.get(function(req, res) {
+		async.waterfall([
+			getCategories
+		], function(err, categories) {
+			if (err) return err;
+			res.render('new', {
+				title: "Cookbook",
+				categories: categories,
+			})
+		})
+	})
+	.post(function(req, res) {
+		var b = req.body
+		if (b.title && b.category && b.ingridients && b.content) {
+			var recipeObj = new Recipe({
+				title: b.title,
+				_category: b.category,
+				ingridients: b.ingridients.split(','),
+				content: b.content,
+				date: Date.now()
+			});
+			recipeObj.save(function(err) {
+				if (err) return err;
+				getRecipeByID(recipeObj._id, null, function(err, recipe) {
+					if (err) return err;
+					res.redirect('/' + recipe._category._id + '/' +
+						recipe._id);
+				});
+			})
+		} else {
+			res.redirect('/new')
+		}
+	})
+router.get('/:categoryID', function(req, res) {
+	async.parallel([
+
+			getCategories,
+			function(callback) {
+				getRecipesByCategory(req.param('categoryID'), function(err, recipes) {
+					if (err) return err;
+					callback(null, recipes);
+				});
+			}
+		],
+		function(err, results) {
+			if (err) return err;
+			res.render('index', {
+				title: "Cookbook",
+				categories: results[0],
+				recipes: results[1]
+			})
+		})
+});
+router.route('/:_category/:_id')
+	.get(function(req, res) {
+		async.parallel([
+
+				getCategories,
+				function(callback) {
+					getRecipeByID(req.param('_id'), req.param('_category'), function(err, recipe) {
+						if (err) return err;
+						callback(null, recipe)
+					})
+				}
+			],
+			function(err, results) {
+				if (err) return err;
+				if (results[1]) {
+					res.render('recipe', {
+						title: "Cookbook",
+						categories: results[0],
+						recipes: results[1]
+					})
+				} else {
+					res.redirect('/');
+				}
+			})
+	})
+	.delete(function(req, res) {
+		Recipe.remove({
+			_id: req.param('_id')
+		}, function(err) {
+			if (err) return err;
+			res.redirect('/');
+		})
+	});
+router.route('/:categoryID/:_id/edit')
+	.get(function(req, res) {
+		async.parallel([
+
+				getCategories,
+				function(callback) {
+					getRecipeByID(req.param('_id'), req.param('categoryID'), function(err, recipe) {
+						if (err) return err;
+						callback(null, recipe)
+					})
+				}
+			],
+			function(err, results) {
+				if (err) return err;
+				if (results[1]) {
+					res.render('edit', {
+						title: "Cookbook",
+						categories: results[0],
+						recipes: results[1]
+					})
+				} else {
+					res.redirect('/');
+				}
+			})
+	})
+	.put(function(req, res) {
+		var b = req.body;
+		if (b.title && b.category && b.ingridients && b.content) {
+			Recipe.update({
+				_id: req.param('_id')
+			}, {
+				title: b.title,
+				_category: b.category,
+				ingridients: b.ingridients.split(','),
+				content: b.content,
+				date: Date.now()
+			}, function(err) {
+				if (err) return err;
+				getRecipeByID(req.param('_id'), b.category, function(err, recipe) {
+					if (err) return err;
+					res.redirect('/' + recipe._category._id + '/' +
+						recipe._id);
+				});
+			});
+		} else {
+			res.redirect('/' + req.param('categoryID') + '/' +
+				req.param('_id') + '/' + 'edit')
+		}
+	})
+router.get(/.+/, function(req, res) {
+	res.redirect('/');
 });
 
-module.exports = router;
+function getCategories(callback) {
+	Category
+		.find({})
+		.select('_id title')
+		.sort('_id')
+		.exec(
+			function(err, categories) {
+				if (err) return err;
+				callback(null, categories);
+			});
+};
+
+function getRecipeByID(id, categoryID, callback) {
+	Recipe
+		.findOne({
+			_id: id
+		})
+		.populate({
+			path: '_category',
+			match: {
+				_id: categoryID,
+			},
+			select: 'title'
+		})
+		.exec(callback)
+};
+
+function getRecipesByCategory(category, callback) {
+	Recipe.find({})
+		.populate({
+			path: '_category',
+			match: {
+				_id: category,
+			},
+			select: 'title'
+		})
+		.select('title _category')
+		.limit(7)
+		.sort('-date')
+		.exec(callback);
+}
+
+module.exports.router = router;
